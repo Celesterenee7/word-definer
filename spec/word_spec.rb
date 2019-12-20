585b740978cc7f8129510b8614e34cf17ec2573a
Celeste Morton <creneemorton@gmail.com>
2019-12-20 09:14:58 -0800

    add the second spec for word, fail, and then pass

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -10,10 +10,19 @@ before(:each) do
 
   describe('#==') do
   it("is the same word if it has the same attributes as another word") do
-    word = Word.new("Intense", nil)
-    word2 = Word.new("Intense", nil)
+    word = Word.new("intense", nil)
+    word2 = Word.new("intense", nil)
     expect(word).to(eq(word2))
   end
  end
+
+ describe('#update') do
+ it("updates a word by id") do
+   word = Word.new("capricious", nil)
+   word.save()
+   word.update("inconstant")
+   expect(word.name).to(eq("inconstant"))
+ end
+end
 
 end
