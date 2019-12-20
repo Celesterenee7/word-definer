94b0e118f9e0fa9a9de88457561866d7f9083425
Celeste Morton <creneemorton@gmail.com>
2019-12-20 09:35:45 -0800

    add the class for definition

---
+++ lib/definition.rb
@@ -0,0 +1,14 @@
+class Definition
+    attr_reader :id
+    attr_accessor :name, :word_id
+
+    @@definitions = {}
+    @@total_rows = 0
+
+    def initialize(name, word_id, id)
+      @name = name
+      @word_id = word_id
+      @id = id || @@total_rows += 1
+    end
+
+end
