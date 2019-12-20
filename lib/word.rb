98500cd14da2776522280c3cddefc6322e48cfe8
Celeste Morton <creneemorton@gmail.com>
2019-12-20 09:04:50 -0800

    add logic to the word class

--- lib/word.rb
+++ lib/word.rb
@@ -5,6 +5,41 @@ class Word
     @@total_rows = 0
 
     def initialize(name, id)
-        @name = name@id = id || @@total_rows += 1
+        @name = name
+        @id = id || @@total_rows += 1
     end
+
+    def update(name)
+        @name = name
+      end
+    
+      def save
+        @@words[self.id] = Word.new(self.name, self.id)
+      end
+
+      def ==(word_to_compare)
+        self.name() == word_to_compare.name()
+      end
+
+      def self.all
+        @@words.values()
+      end
+
+      def self.clear
+        @@words = {}
+        @@total_rows = 0
+      end
+
+      def self.find(id)
+        @@words[id]
+      end
+
+      def delete
+        @@words.delete(self.id)
+      end
+
+    #   def definitions
+    #     Definition.find_by_word(self.id)
+    #   end
+    
 end
